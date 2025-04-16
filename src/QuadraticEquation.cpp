9f201422fd89fba162e35fbcc6ebda24ac7842e2
Anatolii Nikulin <a.nikulin@rdp.ru>
2025-04-16 21:18:00 +0300

    Update QuadraticEquation::solve
     - solving case "x^2+1 = 0"

--- src/QuadraticEquation.cpp
+++ src/QuadraticEquation.cpp
@@ -1,8 +1,16 @@
 #include "QuadraticEquation.h"
 
 #include <vector>
+#include <cmath>
 
 std::vector<double> QuadraticEquation::solve(double a, double b, double c)
 {
+    if (FP_ZERO == std::fpclassify(b))
+    {
+        if ((0 - c/a) < 0)
+        {
+            return {};
+        }
+    }
     return {0, 0};
 }
