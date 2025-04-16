03fc39e25f29c7f605274a7d400da64f4f660504
Anatolii Nikulin <a.nikulin@rdp.ru>
2025-04-16 20:45:11 +0300

    Initial commit

---
+++ src/QuadraticEquation.cpp
@@ -0,0 +1,8 @@
+#include "QuadraticEquation.h"
+
+#include <vector>
+
+std::vector<double> QuadraticEquation::solve(double a, double b, double c)
+{
+    return {};
+}
---
+++ tests/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    auto result = RUN_ALL_TESTS();
+
+    return result;
+}
