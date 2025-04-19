9b6ca6914cdb39ffdf0e36c805d40ae94976657e
Anatolii Nikulin <a.nikulin@rdp.ru>
2025-04-19 20:40:47 +0300

    Update QuadraticEquation::solve
        - solving case "x^2-1 = 0"

--- src/QuadraticEquation.cpp
+++ src/QuadraticEquation.cpp
@@ -7,10 +7,18 @@ std::vector<double> QuadraticEquation::solve(double a, double b, double c)
 {
     if (FP_ZERO == std::fpclassify(b))
     {
-        if ((0 - c/a) < 0)
+        double tmp = 0 - c/a;
+
+        if (tmp < 0)
         {
             return {};
         }
+        else
+        {
+            auto x = sqrtf(tmp);
+            return {x, -x};
+        }
     }
+
     return {0, 0};
 }
