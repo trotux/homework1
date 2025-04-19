038ae240c558b25cc44c7b7823ee28671430575f
Anatolii Nikulin <a.nikulin@rdp.ru>
2025-04-19 20:39:57 +0300

    Add test #2
        - add test for case "x^2-1 = 0"

--- tests/main.cpp
+++ tests/main.cpp
@@ -11,6 +11,16 @@ TEST(QE, Case_1_0_1)
     EXPECT_EQ(roots.size(), 0);
 }
 
+TEST(QE, Case_1_0_n1)
+{
+    auto roots = QuadraticEquation::solve(1, 0, -1);
+
+    EXPECT_EQ(roots.size(), 2);
+    EXPECT_DOUBLE_EQ(roots[0], 1);
+    EXPECT_DOUBLE_EQ(roots[1], -1);
+}
+
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
