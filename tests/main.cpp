cb0d7c549c73bd071070e1b823a59b964fc29efc
Anatolii Nikulin <a.nikulin@rdp.ru>
2025-04-19 21:15:51 +0300

    Add test #4
        - add test if epsilon = 0.001

--- tests/main.cpp
+++ tests/main.cpp
@@ -34,6 +34,16 @@ TEST(QE, Case_0_2_1)
     EXPECT_ANY_THROW(auto roots = QuadraticEquation::solve(0, 2, 1));
 }
 
+TEST(QE, Case_1_2_09999)
+{
+    auto roots = QuadraticEquation::solve(1, 2, 0.9999);
+
+    EXPECT_EQ(roots.size(), 2);
+    EXPECT_DOUBLE_EQ(roots[0], roots[1]);
+    EXPECT_DOUBLE_EQ(roots[1], -1);
+}
+
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
